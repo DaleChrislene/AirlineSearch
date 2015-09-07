a87ca369431a149a44ebb2dff74a87e560ad4a21
DaleC <summer.dchrislene@gmail.com>
2015-09-07 23:59:32 +0800

    Updating functionality

--- public/core.js
+++ public/core.js
@@ -1,21 +1,36 @@
 var airlineSearch = angular.module('airlineSearch',[]);
 
-function searchController($scope, $http){
+function SearchController($scope, $http){
 	$scope.formData = {};
+	$scope.hasAirline = "true";
+	$scope.titleFrom = "";
+	$scope.titleTo = "";
+	$scope.init = false;
 
 	$scope.searchAirlines = function(){
-		$http.post('http://jsonplaceholder.typicode.com/posts',$scope.formData)
+		$http.post('http://localhost:3000/api/v1/search',$scope.formData)
 		.success(function(data){
+			$scope.titleFrom = capitalize($scope.formData.from);
+			$scope.titleTo = capitalize($scope.formData.to);
 			$scope.formData = {};
+			$scope.init = true;
 
-			// FOR TESTING
-			data={"airlines": [ "Singapore Airlines", "Malaysian Airlines", "Cathay Pacific", "China Air" ]}
-
-			$scope.airlineList = data['airlines'];
-			console.log($scope.airlineList);
+			airlineList = data['airlines'];
+			if(airlineList == null){
+				$scope.hasAirline = "false";
+				$scope.airlineList = [];
+			}
+			else{
+				$scope.hasAirline = "true";
+				$scope.airlineList = airlineList['airline'];
+			}
 		})
 		.error(function(data){
 			console.log('Error: ' + data);
 		})
 	}
+}
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
