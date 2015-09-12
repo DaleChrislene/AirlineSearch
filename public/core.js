11fb874443f9fb0aabee19eb856dc44aede70ddc
DaleC <summer.dchrislene@gmail.com>
2015-09-12 19:59:14 +0800

    Handling different response codes

--- public/core.js
+++ public/core.js
@@ -16,9 +16,9 @@ angular.module('airlineSearch').controller('SearchController', function SearchCo
 		$('body').addClass("loading");
 		$scope.init = true;
 
-		$http.post(searchApiUrl+'api/v1/search',$scope.formData)
+		$http.post(searchApiUrlLocal +'api/v1/search',$scope.formData)
 		.success(function(data){
-
+			console.log(data);
 			$('body').removeClass("loading");
 
 			//Setting data to list title
@@ -28,21 +28,33 @@ angular.module('airlineSearch').controller('SearchController', function SearchCo
 			//clear form
 			$scope.formData = {};
 			
-			airlineList = data['airlines'];
-			console.log(airlineList);
-			$scope.airlineList = setResultList(airlineList);
-			console.log($scope.airlineList);
-			//set variable to show/hide error message
-			if($scope.airlineList.length === 0){
-				$scope.hasAirline = false;
+			console.log(data['status']);
+			if(data["status"] == '200')
+			{
+				responseObj = JSON.parse(data['response']);
+				console.log(responseObj);
+				airlineList = responseObj['airlines']
+				console.log(airlineList);
+				$scope.airlineList = setResultList(airlineList);
+				console.log($scope.airlineList);
+
+				//set variable to show/hide error message
+				if($scope.airlineList.length === 0){
+					$scope.hasAirline = false;
+				}
+				else{
+					$scope.hasAirline = true;
+				}
 			}
-			else{
+			else if(data['status'] == 500){
+				alert("Our Search Service isn't available. Please try later.");
 				$scope.hasAirline = true;
+				$scope.airlineList = [];
 			}
-			
 		})
-		.error(function(data){
+		.error(function(data){			
 			$('body').removeClass("loading");
+			alert("Service currently unavailable. Please try later.");
 		})
 	}
 
